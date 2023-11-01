518fc8b74b46c77358b0a09595fff57f42feeec9
tmdghtmdgh99 <tmdghtmdgh99@naver.com>
2023-11-01 22:36:20 +0900

    [Add] BOJ_2467_Yoo_60, [Add] BOJ_2470_Yoo_60

---
+++ WEEK_13/BOJ_2470_두용액_Yoo.java
@@ -0,0 +1,61 @@
+package 용액;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 두용액 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int [] arr= new int [N];
+		
+		for(int i=0; i<N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		//시작과 끝 인덱스
+		int start = 0;
+		int end = N-1;
+		
+		//답이 될 용액 두개
+		int ans1 = 0;
+		int ans2 = 0;
+		
+		//최대값으로 초기 설정
+		int min = Integer.MAX_VALUE;
+		
+		//투포인터에서 앞 인덱스가 뒤 인덱스랑 같아지면 끝!
+		while (start<end) {
+			int check = Math.abs(arr[start]+arr[end]);
+			
+			//최솟값 발견! min 값 갱신, ans 값 저장!
+			if(check<min) {
+				min = check;
+				ans1 = arr[start];
+				ans2 = arr[end];
+			}
+			
+			//0이면 무조건 답!
+			if(check==0) {
+				break;
+			}
+			
+			// 최대한 0에 가깝게 하는 부분
+			// 더했는데 0보다 크면 오른쪽(큰 값) 을 줄여줘야겠죠?  
+			// 음수면 최소값을 줄여줘야겠죠
+			if(arr[start]+arr[end]>0) {
+				end--;
+			}
+			else {
+				start++;
+			}
+		}
+		System.out.println(ans1+" "+ans2);
+		
+	}
+}
